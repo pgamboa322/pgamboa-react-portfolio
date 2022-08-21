236aac3ec97e5cc113a47c0c30eb1ffb7448d9d4
pgamboa322 <pgamboa322@gmail.com>
2022-08-21 15:05:12 -0500

    Added in final component for portfolio. need to add resume.

--- pgamboa-react-portfolio/src/Pages/Home.js
+++ pgamboa-react-portfolio/src/Pages/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
 // import HomeSection from "../Components/HomeSection/HomeSection";
 import AboutSection from "../Components/About-Section/AboutSection";
-// import MyWork from "../Components/MyWork/MyWork";
+import Work from "../Components/My-Work/Work";
 import Overview from '../Components/Overview/Overview';
 import Description from "../Description/Description";
-// import Contact from '../Components/Contact/Contact';
+import Contact from "../Components/Contact/Contact";
 
 function Home() {
     return(
         <div>
         <Overview/>
         <Description/>
-        {/* <MyWork/> */}
+        <Work/>
         <AboutSection/>
-        {/* <Contact/> */}
+        <Contact/>
         </div>
     );
 };
